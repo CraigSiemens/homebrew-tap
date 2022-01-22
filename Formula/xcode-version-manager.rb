054b444561f7211c48b818f39becea1ca4c0098d
Craig Siemens <siemens.craig@gmail.com>
2022-01-21 18:09:44 -0700

    Created the xcode-version-manager formula

---
+++ Formula/xcode-version-manager.rb
@@ -0,0 +1,32 @@
+class XcodeVersionManager < Formula
+  desc "Manage multiple installed versions of Xcode"
+  homepage "https://github.com/CraigSiemens/xcode-version-manager"
+  url "https://github.com/CraigSiemens/xcode-version-manager.git", tag: "0.0.0"
+  license "MIT"
+  head "https://github.com/CraigSiemens/xcode-version-manager.git", branch: "main"
+
+  depends_on xcode: ["12.5", :build]
+  depends_on macos: :catalina
+
+  def install
+    system "make", "build", "completions"
+    bin.install ".build/release/xcvm"
+
+    bash_completion.install ".build/completions/bash" => "xcvm"
+    zsh_completion.install ".build/completions/zsh" => "_xcvm"
+    fish_completion.install ".build/completions/fish" => "xcvm.fish"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test xcode-version-manager`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
