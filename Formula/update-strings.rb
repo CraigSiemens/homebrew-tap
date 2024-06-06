b9c41c9ecea7b045a25419709334554669cf124d
Craig Siemens <680442+CraigSiemens@users.noreply.github.com>
2024-06-06 23:39:56 +0000

    update-strings: add 1.2.1 bottle.

--- Formula/update-strings.rb
+++ Formula/update-strings.rb
@@ -7,6 +7,12 @@ class UpdateStrings < Formula
   license "MIT"
   head "https://github.com/CraigSiemens/update-strings.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/CraigSiemens/homebrew-tap/releases/download/update-strings-1.2.1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "be4933d5c7ede0b921c925583e4388690888916531283e88bb342496b49c8143"
+  end
+
   depends_on xcode: ["12.5", :build]
   uses_from_macos "swift"
 
