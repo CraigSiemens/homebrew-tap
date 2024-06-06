2784e7038a700a7b8037e621ff620246226d9fbd
Craig Siemens <siemens.craig@gmail.com>
2024-06-06 16:29:40 -0600

    Fixed failures in the xcode-version-manager formula.
    Closes #17.
    
    Signed-off-by: Craig Siemens <680442+CraigSiemens@users.noreply.github.com>

--- Formula/xcode-version-manager.rb
+++ Formula/xcode-version-manager.rb
@@ -1,9 +1,9 @@
 class XcodeVersionManager < Formula
   desc "Manage multiple installed versions of Xcode"
   homepage "https://github.com/CraigSiemens/xcode-version-manager"
-  url "https://github.com/CraigSiemens/xcode-version-manager.git", 
-    tag: "0.7.0",
-    revision: "4dac402225c697f197f4a8dcc5e5f4e254faa300"
+  url "https://github.com/CraigSiemens/xcode-version-manager.git",
+      tag:      "0.7.0",
+      revision: "4dac402225c697f197f4a8dcc5e5f4e254faa300"
   license "MIT"
   head "https://github.com/CraigSiemens/xcode-version-manager.git", branch: "main"
 
@@ -17,15 +17,6 @@ class XcodeVersionManager < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test xcode-version-manager`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "#{bin}/xcvm", "-h"
   end
 end
