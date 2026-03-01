e092886780a967f785407caf6939ddb7ef66c8c0
Craig Siemens <680442+CraigSiemens@users.noreply.github.com>
2026-03-01 23:00:03 +0000

    xcode-version-manager: update 1.6.0 bottle.

--- Formula/xcode-version-manager.rb
+++ Formula/xcode-version-manager.rb
@@ -8,8 +8,8 @@ class XcodeVersionManager < Formula
   head "https://github.com/CraigSiemens/xcode-version-manager.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/CraigSiemens/homebrew-tap/releases/download/xcode-version-manager-1.5.4"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2496d7c566c290c84249e8b381d6d8f095b56b182a23ca1cdad821627833588e"
+    root_url "https://github.com/CraigSiemens/homebrew-tap/releases/download/xcode-version-manager-1.6.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e272582ec810903af68ea80722d58f4db2f5906f1099674699826905673f121e"
   end
 
   depends_on xcode: ["15.0", :build]
